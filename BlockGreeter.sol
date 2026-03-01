fe66e29ce6924306f1c5b472839499aaec96eaff
Gojo <vishalvc16vc@gmail.com>
2026-03-01 23:59:01 +0530

    Change greeting from 'gm Base' to 'gn Base'

--- BlockGreeter.sol
+++ BlockGreeter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 contract BlockGreeter {
-    string public greeting = "gm Base";
+    string public greeting = "gn Base";
 
     function setGreeting(string calldata _g) external {
         greeting = _g;
